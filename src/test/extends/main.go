54ac72a3227a2d238b702ea4afc5a3f8d0a5f095
liuyang <944491509@qq.com>
2020-07-25 16:28:36 +0800

    结构体继承

--- src/test/extends/main.go
+++ src/test/extends/main.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
+// 定义个动物
 type animal struct {
+	name string
 }
 
+// 给动物实现一个移动的方法
+func (a animal) move() {
+	fmt.Printf("%s会动\n", a.name)
+}
+
+// 小狗
 type dog struct {
-	animal
-	feet int
+	animal // animal 拥有的方法dog都拥有
+	feet   int
 }
 
 // 狗在叫
 func (d dog) wang() {
-	fmt.Println("%s在叫:汪汪汪~", d.name)
+	fmt.Printf("%s在叫:汪汪汪~\n", d.name)
+}
+
+func main() {
+	d1 := dog{
+		animal: animal{
+			name: "小白",
+		},
+		feet: 2,
+	}
+	fmt.Println(d1)
+	d1.move()
+	d1.wang()
 }
