0e5871fbca150a43c27475663c632b99b1673e94
liuyang <944491509@qq.com>
2020-07-26 15:28:13 +0800

    方法和接收者

--- src/test/struct_json/main.go
+++ src/test/struct_json/main.go
@@ -15,6 +15,22 @@ type person struct {
 	Age  int    `json:"age"`
 }
 
+// 沟造（结构体变量的）函数， 返回值是对应的结构体类型
+func newPerson(n string, i int) (p person) {
+	p = person{
+		Name: n,
+		Age:  i,
+	}
+	return p
+}
+
+// 方法
+// 接收者使用对应类型的首字母小写
+// 指定来接收者之后，只有接受者才能调用这个方法
+func (p person) dream(str string) {
+	fmt.Printf("%s的梦想是%s\n", p.Name, str)
+}
+
 func main() {
 	p1 := person{
 		Name: "刘洋",
@@ -32,4 +48,10 @@ func main() {
 	// 传指针是为了能在json.Unmarshal内部修改p2的值
 	json.Unmarshal([]byte(str), &p2)
 	fmt.Printf("%v\n", p2)
+
+	p3 := person{"张朋", 26}
+	p4 := newPerson("老曹", 27)
+	fmt.Println(p3, p4)
+	p1.dream("学好go语言")
+	p2.dream("学好java")
 }
