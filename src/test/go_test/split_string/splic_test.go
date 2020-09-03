fd442a0249dcaf5a19a0b352ca2a43a6ac9c81b9
liuyang <944491509@qq.com>
2020-09-03 14:49:13 +0800

    测试分组 和 测试组名称

--- src/test/go_test/split_string/splic_test.go
+++ src/test/go_test/split_string/splic_test.go
@@ -6,29 +6,41 @@ import (
 )
 
 func TestSplit(t *testing.T) {
-	got := Split("abcdbcdesbcdr", "b")
-	want := []string{"a", "cd", "cdes", "cdr"}
-	// 切换比较
-	if !reflect.DeepEqual(got, want) {
-		// 测试失败
-		t.Errorf("want:%v but got:%v\n", want, got)
+	type testCase struct {
+		str  string
+		sep  string
+		want []string
 	}
-}
-
-func Test2Split(t *testing.T) {
-	got := Split("a:b:c:d:e", ":")
-	want := []string{"a", "b", "c", "d", "e"}
-	if !reflect.DeepEqual(got, want) {
-		// 测试失败
-		t.Errorf("want:%v but got:%v\n", want, got)
+	// 测试分组
+	testGroup := []testCase{
+		{"abcdbcdesbcdr", "b", []string{"a", "cd", "cdes", "cdr"}},
+		{"a:b:c:d:e", ":", []string{"a", "b", "c", "d", "e"}},
+		{"abcedsbcasd", "bc", []string{"a", "eds", "asd"}},
+		{"小洋爱小鑫", "爱", []string{"小洋", "小鑫"}},
 	}
-}
 
-func Test3Split(t *testing.T) {
-	got := Split("abcedsbcasd", "bc")
-	want := []string{"a", "eds", "asd"}
-	if !reflect.DeepEqual(got, want) {
-		// 测试失败
-		t.Errorf("want:%v but got:%v\n", want, got)
+	testGroup1 := map[string]testCase{
+		"test1": {"abcdbcdesbcdr", "b", []string{"a", "cd", "cdes", "cdr"}},
+		"test2": {"a:b:c:d:e", ":", []string{"a", "b", "c", "d", "e"}},
+		"test3": {"abcedsbcasd", "bc", []string{"a", "eds", "asd"}},
+		"test4": {"小洋爱小鑫", "爱", []string{"小洋", "小鑫"}},
+	}
+
+	// go test -run=TestSplit/test4  单独跑一个测试的命令
+	for _, tc := range testGroup {
+		got := Split(tc.str, tc.sep)
+		if !reflect.DeepEqual(tc.want, got) {
+			// 测试失败
+			t.Errorf("want:%v but got:%v\n", tc.want, got)
+		}
+	}
+
+	for name, tc := range testGroup1 {
+		t.Run(name, func(t *testing.T) {
+			got := Split(tc.str, tc.sep)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("want:%v but got:%v\n", tc.want, got)
+			}
+		})
 	}
 }
